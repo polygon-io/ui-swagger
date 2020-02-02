7f3c465cb844df39dbdc07ffaff0f8f927274f36
julien prugne <julien@webeleon.dev>
2020-02-02 13:16:02 +0100

    #11 & #12 & #18 set boolean and enums as dropdowns

--- app/components/TaggedOperations/RequestParameters.js
+++ app/components/TaggedOperations/RequestParameters.js
@@ -1,19 +1,55 @@
 import React from "react";
 import Markdown from "react-markdown";
 
-const Parameter = ({ parameter, setParameter, values }) => {
+const Parameter = ({ parameter, setParameter, values, ...props }) => {
   const { name, type, description } = parameter;
+  let input = (
+    <input
+      className="input"
+      type={type}
+      value={values[name] || ""}
+      onChange={event => setParameter(name, event.target.value)}
+    />
+  );
+  if (type === "boolean") {
+    input = (
+      <div className="select parameter__select">
+        <select
+          className="select parameter__select"
+          value={values[name] || ""}
+          onChange={event => setParameter(name, event.target.value)}
+        >
+          <option value=""></option>
+          <option value="true">true</option>
+          <option value="false">false</option>
+        </select>
+      </div>
+    );
+  }
+  if (parameter.enum && parameter.enum.length > 0) {
+    input = (
+      <div className="select parameter__select">
+        <select
+          className="select parameter__select"
+          value={values[name] || ""}
+          onChange={event => setParameter(name, event.target.value)}
+        >
+          <option value=""></option>
+          {parameter.enum.map(e => (
+            <option key={e} value={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
   return (
     <div className="parameter__box">
       <div className="columns">
         <div className="column is-one-quarter">{name}</div>
         <div className="column">
-          <input
-            className="input"
-            type={type}
-            value={values[name] || ""}
-            onChange={event => setParameter(name, event.target.value)}
-          />
+          {input}
           <p className="parameter__type">{type}</p>
           <Markdown source={description} />
         </div>
