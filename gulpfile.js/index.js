1931d6eb8fa81baffda7af11195d708b6c7b3b74
Andrew Bailey <alb@andrewbailey.me>
2020-04-30 13:46:09 -0400

    fromatting

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,41 +1,41 @@
-"use strict";
+'use strict';
 
 // passed in ops:
-const gulp = require("gulp");
-const path = require("path");
-const lodash = require("lodash");
-const browserSync = require("browser-sync");
-const gulpSequence = require("gulp-sequence").use(gulp);
-const program = require("commander");
-const requireDir = require("require-dir");
+const gulp = require('gulp');
+const path = require('path');
+const lodash = require('lodash');
+const browserSync = require('browser-sync');
+const gulpSequence = require('gulp-sequence').use(gulp);
+const program = require('commander');
+const requireDir = require('require-dir');
 
 var config = {
-  app: "./app",
-  dest: "./public",
+  app: './app',
+  dest: './public',
   uiPort: 5005,
   tasks: {
     sass: {
-      dest: "docs/styles",
-      paths: ["app/styles", "node_modules/", "node_modules/font-awesome/scss"],
+      dest: 'docs/styles',
+      paths: ['app/styles', 'node_modules/', 'node_modules/font-awesome/scss'],
       artifacts: {
         app: {
-          src: ["app/styles/app.scss", "app/**/*.scss"]
+          src: ['app/styles/app.scss', 'app/**/*.scss']
         }
       }
     },
     webpack: {
-      src: "",
-      dest: "docs/",
+      src: '',
+      dest: 'docs/',
       artifacts: {
-        app: "./app"
+        app: './app'
       },
-      output: "app.bundle.js",
+      output: 'app.bundle.js',
       rules: [
         {
           test: /\.html$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: "html-loader?attrs=false",
+            loader: 'html-loader?attrs=false',
             options: {
               attrs: false
             }
@@ -44,20 +44,20 @@ var config = {
         {
           test: /\.js$/,
           exclude: /(node_modules|bower_components)/,
-          include: [path.resolve(__dirname, "../", "app")],
+          include: [path.resolve(__dirname, '../', 'app')],
           use: {
-            loader: "babel-loader", // 'babel-loader' is also a legal name to reference
+            loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
             options: {
-              presets: ["@babel/preset-env", "@babel/react"],
+              presets: ['@babel/preset-env', '@babel/react'],
               plugins: [
                 [
-                  "@babel/plugin-proposal-decorators",
+                  '@babel/plugin-proposal-decorators',
                   {
                     legacy: true
                   }
                 ],
                 [
-                  "@babel/plugin-proposal-class-properties",
+                  '@babel/plugin-proposal-class-properties',
                   {
                     loose: true
                   }
@@ -69,39 +69,39 @@ var config = {
       ]
     },
     html: {
-      src: "*.html"
+      src: '*.html'
     },
     images: {
-      src: ["app/images/**/*.+(png|jpg|gif|jpeg)"],
-      dest: "docs/images"
+      src: ['app/images/**/*.+(png|jpg|gif|jpeg)'],
+      dest: 'docs/images'
     },
     webp: {
-      src: ["app/images/**/*.+(png|jpg|gif|jpeg)"],
-      dest: "docs/images"
+      src: ['app/images/**/*.+(png|jpg|gif|jpeg)'],
+      dest: 'docs/images'
     },
     fonts: {
-      src: ["app/fonts/*"],
-      dest: "docs/fonts"
+      src: ['app/fonts/*'],
+      dest: 'docs/fonts'
     },
     icons: {
-      template: "app/images/icons/template.css",
-      src: ["app/images/icons/*.svg"],
-      dest: "docs/icons"
+      template: 'app/images/icons/template.css',
+      src: ['app/images/icons/*.svg'],
+      dest: 'docs/icons'
     },
     svgs: {
-      src: ["app/images/*.svg", "app/images/**/*.svg"],
-      dest: "docs/images"
+      src: ['app/images/*.svg', 'app/images/**/*.svg'],
+      dest: 'docs/images'
     }
   }
 };
 
 program
-  .version("0.1.1")
-  .option("-d, --dontbuild", "Dont build the app before running the tests")
-  .option("-F, --fix", "Attempt to fix the lint errors when running")
+  .version('0.1.1')
+  .option('-d, --dontbuild', 'Dont build the app before running the tests')
+  .option('-F, --fix', 'Attempt to fix the lint errors when running')
   .parse(process.argv);
 
-let env = "dev";
+let env = 'dev';
 const falsy = /^(?:f(?:alse)?|no?|0+)$/i;
 Boolean.parse = val => {
   return !falsy.test(val) && !!val;
@@ -120,7 +120,7 @@ ops.prod = function() {
 ops.env = env;
 
 // Require all tasks in gulpfile.js/tasks, including subfolders
-let tasks = requireDir("./tasks");
+let tasks = requireDir('./tasks');
 
 // load each gulp task and pass it ops:
 tasks = lodash.each(tasks, task => task(ops));
