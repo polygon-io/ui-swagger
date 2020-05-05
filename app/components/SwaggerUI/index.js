e91fc131813456e06e925b6016cae3dea7ca6385
Andrew Bailey <alb@andrewbailey.me>
2020-05-05 15:19:03 -0400

    formatting

--- app/components/SwaggerUI/index.js
+++ app/components/SwaggerUI/index.js
@@ -1,70 +1,70 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { SideBar } from "../SideBar";
-import { GettingStarted } from "../GettingStarted";
-import { TaggedOperations } from "../TaggedOperations";
+import { SideBar } from '../SideBar';
+import { GettingStarted } from '../GettingStarted';
+import { TaggedOperations } from '../TaggedOperations';
 
-@connect(store => ({
-  user: store.user
+@connect((store) => ({
+	user: store.user,
 }))
 export class SwaggerUI extends React.Component {
-  componentDidMount() {
-    const anchoredElement = document.getElementById(
-      window.location.hash.replace("#", "")
-    );
-    if (anchoredElement) {
-      anchoredElement.scrollIntoView();
-    }
-  }
+	componentDidMount() {
+		const anchoredElement = document.getElementById(
+			window.location.hash.replace('#', '')
+		);
+		if (anchoredElement) {
+			anchoredElement.scrollIntoView();
+		}
+	}
 
-  render() {
-    const { swaggerClient } = this.props;
-    const { paths } = swaggerClient.spec;
+	render() {
+		const { swaggerClient } = this.props;
+		const { paths } = swaggerClient.spec;
 
-    const orderedOperations = Object.values(
-      Object.entries(paths).reduce((carry, item) => {
-        const [path, operations] = item;
-        const tags = operations.get.tags;
+		const orderedOperations = Object.values(
+			Object.entries(paths).reduce((carry, item) => {
+				const [path, operations] = item;
+				const tags = operations.get.tags;
 
-        // loop over tags to determine what list(s) to assign to
-        tags.map(tag => {
-          if (!carry[tag]) {
-            carry[tag] = {
-              tag,
-              operations: {
-                [path]: {
-                  path,
-                  ...operations
-                }
-              }
-            };
-          } else {
-            carry[tag].operations[path] = {
-              path,
-              ...operations
-            };
-          }
-        });
+				// loop over tags to determine what list(s) to assign to
+				tags.map((tag) => {
+					if (!carry[tag]) {
+						carry[tag] = {
+							tag,
+							operations: {
+								[path]: {
+									path,
+									...operations,
+								},
+							},
+						};
+					} else {
+						carry[tag].operations[path] = {
+							path,
+							...operations,
+						};
+					}
+				});
 
-        return carry;
-      }, {})
-    );
-    return (
-      <div className="columns">
-        <SideBar
-          className="column"
-          orderedOperations={orderedOperations}
-          {...this.props}
-        />
-        <div className="column">
-          <GettingStarted id="getting-started" />
-          <TaggedOperations
-            orderedOperations={orderedOperations}
-            {...this.props}
-          />
-        </div>
-      </div>
-    );
-  }
+				return carry;
+			}, {})
+		);
+		return (
+			<div className="columns">
+				<SideBar
+					className="column"
+					orderedOperations={orderedOperations}
+					{...this.props}
+				/>
+				<div className="column">
+					<GettingStarted id="getting-started" />
+					<TaggedOperations
+						orderedOperations={orderedOperations}
+						{...this.props}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
