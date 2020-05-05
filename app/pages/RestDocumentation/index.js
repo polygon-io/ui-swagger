b1cab274fc1053aebda19048c517af4775f8c188
Andrew Bailey <alb@andrewbailey.me>
2020-05-05 14:29:26 -0400

    Formating

--- app/pages/RestDocumentation/index.js
+++ app/pages/RestDocumentation/index.js
@@ -1,55 +1,54 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import SwaggerClient from "swagger-client";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SwaggerClient from 'swagger-client';
 
-import actions from "./../../actions/";
-import { SwaggerUI } from "../../components/SwaggerUI";
+import actions from './../../actions/';
+import { SwaggerUI } from '../../components/SwaggerUI';
 
-@connect(store => ({
-  app: store.app,
-  router: store.router,
-  location: store.location
+@connect((store) => ({
+	app: store.app,
+	router: store.router,
+	location: store.location,
 }))
 export class RestDocumentation extends Component {
-  state = {
-    visible: {},
-    swaggerClient: false
-  };
+	state = {
+		visible: {},
+		swaggerClient: false,
+	};
 
-  componentDidMount() {
-    this.props.dispatch(
-      actions.app.update({
-        title: "Polygon.io - Real-time Stock APIs, Forex and Crypto"
-      })
-    );
-    new SwaggerClient(this.props.app.definitionLink).then(swaggerClient => {
-      this.setState({
-        ...this.state,
-        swaggerClient
-      });
-    });
-  }
+	componentDidMount() {
+		this.props.dispatch(
+			actions.app.update({
+				title: 'Polygon.io - Real-time Stock APIs, Forex and Crypto',
+			})
+		);
+		new SwaggerClient(this.props.app.definitionLink).then((swaggerClient) => {
+			this.setState({
+				...this.state,
+				swaggerClient,
+			});
+		});
+	}
 
-  // Render
-  render() {
-    if (!this.state.swaggerClient) {
-      return (
-        <div id="restdocumentation" className="page restdocumentation--loader">
-          <span>
-            <i
-              key="restdocumentation__loader"
-              className="fas fa-circle-notch fa-spin fa-3x"
-            ></i>
-          </span>
-        </div>
-      );
-    }
-    return (
-      <div id="restdocumentation" className="page">
-        <SwaggerUI swaggerClient={this.state.swaggerClient} {...this.props} />
-      </div>
-    );
-  }
+	// Render
+	render() {
+		if (!this.state.swaggerClient) {
+			return (
+				<div id="restdocumentation" className="page restdocumentation--loader">
+					<span>
+						<i
+							key="restdocumentation__loader"
+							className="fas fa-circle-notch fa-spin fa-3x"></i>
+					</span>
+				</div>
+			);
+		}
+		return (
+			<div id="restdocumentation" className="page">
+				<SwaggerUI swaggerClient={this.state.swaggerClient} {...this.props} />
+			</div>
+		);
+	}
 }
 
 export default RestDocumentation;
