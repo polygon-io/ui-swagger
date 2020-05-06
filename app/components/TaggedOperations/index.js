aae21776e04c45c5956d3670525082b68455bb89
Andrew Bailey <alb@andrewbailey.me>
2020-05-06 17:29:43 -0400

    refactor

--- app/components/TaggedOperations/index.js
+++ app/components/TaggedOperations/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
-import { toHTMLId } from '../../helpers/utils';
 import { ApiEndpoints } from './ApiEndpoints';
 
 export const TaggedOperations = ({ orderedOperations, ...props }) => {
-	return orderedOperations.map((taggedOperations) => (
+	return orderedOperations.map((taggedOperations, i) => (
 		<ApiEndpoints
-			key={`operations_${toHTMLId(taggedOperations.tag)}`}
+			key={i}
 			tag={taggedOperations.tag}
 			taggedOperations={taggedOperations}
 			{...props}
