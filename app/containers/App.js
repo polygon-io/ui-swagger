313fc055f08aec42be6ec34868ae2fc3a6d7c7fc
Andrew Bailey <alb@andrewbailey.me>
2020-05-06 16:37:26 -0400

    format

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,47 +1,47 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Route, Switch, withRouter } from "react-router-dom";
-import actions from "../actions/index";
-import Header from "./NavbarContainer";
-import Footer from "./FooterContainer";
-import NotificationsBar from "../components/NotificationsBar";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import actions from '../actions/index';
+import Header from './NavbarContainer';
+import Footer from './FooterContainer';
+import NotificationsBar from '../components/NotificationsBar';
 
 // Pages:
-import { RestDocumentation } from "../pages/RestDocumentation";
-import FourOhFour from "./FourOhFour.js";
+import { RestDocumentation } from '../pages/RestDocumentation';
+import FourOhFour from './FourOhFour.js';
 
-const ConnectedSwitch = connect(store => ({
-  location: store.location
+const ConnectedSwitch = connect((store) => ({
+	location: store.location,
 }))(Switch);
 
-@connect(store => ({
-  app: store.app,
-  location: store.location,
-  notifications: store.notifications
+@connect((store) => ({
+	app: store.app,
+	location: store.location,
+	notifications: store.notifications,
 }))
 class AppContainer extends Component {
-  componentDidMount() {
-    this.props.dispatch(actions.systemstatus.load());
-  }
-  componentWillMount() {
-    this.props
-      .dispatch(actions.user.fetchUser())
-      .catch(err => console.log("User not logged in..."));
-  }
-  render() {
-    return (
-      <div className="app_wrapper">
-        <Header />
-        <ConnectedSwitch>
-          <Route exact path="/" component={RestDocumentation} />
-          <Route exact path="/docs" component={RestDocumentation} />
-          <Route component={FourOhFour}></Route>
-        </ConnectedSwitch>
-        <Footer />
-        <NotificationsBar />
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.props.dispatch(actions.systemstatus.load());
+	}
+	componentWillMount() {
+		this.props
+			.dispatch(actions.user.fetchUser())
+			.catch((err) => console.log('User not logged in...'));
+	}
+	render() {
+		return (
+			<div className="app_wrapper">
+				<Header />
+				<ConnectedSwitch>
+					<Route exact path="/" component={RestDocumentation} />
+					<Route exact path="/docs" component={RestDocumentation} />
+					<Route component={FourOhFour}></Route>
+				</ConnectedSwitch>
+				<Footer />
+				<NotificationsBar />
+			</div>
+		);
+	}
 }
 
 export default withRouter(AppContainer);
