3219d37e75c89fe5806a1f61b9c5c58525c3cb07
Andrew Bailey <alb@andrewbailey.me>
2020-05-11 22:43:24 -0400

    Spinner added to sidebar and update styling

--- app/components/SideBar/index.js
+++ app/components/SideBar/index.js
@@ -1,61 +1,65 @@
-import React from "react";
-import Scrollspy from "react-scrollspy";
+import React from 'react';
+import Scrollspy from 'react-scrollspy';
 
-import { SidebarOperationsSection } from "./OperationSection";
+import { SidebarOperationsSection } from './OperationSection';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import Spinner from '../../helpers/spinner';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 export const SideBar = ({ orderedOperations, user }) => {
-  const apiKeyComponent = user.isLoggedIn ? (
-    <span>Using your API key</span>
-  ) : (
-    <div className="sidebar__log-in-btn-group">
-      <a
-        href="/signup?next=/dashboard/billing/plan"
-        className="button is-primary"
-      >
-        Get your API Key
-      </a>
-      <p className="mt-8">
-        <a href="/signup?next=/dashboard/billing/plan">Sign up</a> or{" "}
-        <a href="/login">Login</a> to get started.
-      </p>
-    </div>
-  );
+	const apiKeyComponent = user.isLoggedIn ? (
+		<span>Using your API key</span>
+	) : (
+		<div className="sidebar__log-in-btn-group">
+			<a
+				href="/signup?next=/dashboard/billing/plan"
+				className="button is-primary">
+				Get your API Key
+			</a>
+			<p className="mt-8">
+				<a href="/signup?next=/dashboard/billing/plan">Sign up</a> or{' '}
+				<a href="/login">Login</a> to get started.
+			</p>
+		</div>
+	);
 
-  return (
-    <aside className="menu">
-      <div className="sidebar is-hidden-mobile">
-        <p className="menu-label title is-5">API Key</p>
-        {user.fetched ? (
-          <div>{apiKeyComponent}</div>
-        ) : (
-          <div className="sidebar_fetch-spinner">
-            <FontAwesomeIcon icon={faCircleNotch} className="fa-spin" />
-            Loading...
-          </div>
-        )}
-        <p className="menu-label title is-5">API Reference</p>
-        <Scrollspy
-          items={["getting-started"]}
-          currentClassName="sidebar__method--current"
-        >
-          <ul className="menu-list">
-            <li key="getting-started">
-              <a href="#getting-started">Getting Started</a>
-            </li>
-          </ul>
-        </Scrollspy>
-        {orderedOperations.map(taggedOperations => {
-          return (
-            <SidebarOperationsSection
-              key={`section_${taggedOperations.tag}`}
-              taggedOperations={taggedOperations}
-            />
-          );
-        })}
-      </div>
-    </aside>
-  );
+	return (
+		<aside className="menu">
+			<div className="sidebar is-hidden-mobile">
+				<p className="menu-label title is-5">API Key</p>
+				{user.fetched ? (
+					<div>{apiKeyComponent}</div>
+				) : (
+					<div className="sidebar_fetch-spinner">
+						<FontAwesomeIcon icon={faCircleNotch} className="fa-spin" />
+						Loading...
+					</div>
+				)}
+				<p className="menu-label title is-5">API Reference</p>
+				<Scrollspy
+					items={['getting-started']}
+					currentClassName="sidebar__method--current">
+					<ul className="menu-list">
+						<li key="getting-started">
+							<a href="#getting-started">Getting Started</a>
+						</li>
+					</ul>
+				</Scrollspy>
+				{orderedOperations.length > 0 ? (
+					orderedOperations.map((taggedOperations) => {
+						return (
+							<SidebarOperationsSection
+								key={`section_${taggedOperations.tag}`}
+								taggedOperations={taggedOperations}
+							/>
+						);
+					})
+				) : (
+					<Spinner />
+				)}
+			</div>
+		</aside>
+	);
 };
