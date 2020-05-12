8a04837bcbb9a2a85211a77689ecc550e7734813
Andrew Bailey <alb@andrewbailey.me>
2020-05-12 09:17:58 -0400

    remove relocated code

--- app/pages/RestDocumentation/index.js
+++ app/pages/RestDocumentation/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import SwaggerClient from "swagger-client";
 
 import actions from "./../../actions/";
 import { SwaggerUI } from "../../components/SwaggerUI";
@@ -11,42 +10,19 @@ import { SwaggerUI } from "../../components/SwaggerUI";
   location: store.location
 }))
 export class RestDocumentation extends Component {
-  state = {
-    visible: {},
-    swaggerClient: false
-  };
-
   componentDidMount() {
     this.props.dispatch(
       actions.app.update({
         title: "Polygon.io - Real-time Stock APIs, Forex and Crypto"
       })
     );
-    new SwaggerClient(this.props.app.definitionLink).then(swaggerClient => {
-      this.setState({
-        ...this.state,
-        swaggerClient
-      });
-    });
   }
 
   // Render
   render() {
-    if (!this.state.swaggerClient) {
-      return (
-        <div id="restdocumentation" className="page restdocumentation--loader">
-          <span>
-            <i
-              key="restdocumentation__loader"
-              className="fas fa-circle-notch fa-spin fa-3x"
-            ></i>
-          </span>
-        </div>
-      );
-    }
     return (
-      <div id="restdocumentation" className="page">
-        <SwaggerUI swaggerClient={this.state.swaggerClient} {...this.props} />
+      <div id="restdocumentation">
+        <SwaggerUI {...this.props} />
       </div>
     );
   }
