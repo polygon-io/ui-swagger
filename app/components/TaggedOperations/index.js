5cf2f9c8ce79dffa4e830eaf735898ab4e9fa39b
Andrew Bailey <alb@andrewbailey.me>
2020-05-11 22:44:43 -0400

    Spinner added to main sections

--- app/components/TaggedOperations/index.js
+++ app/components/TaggedOperations/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { ApiEndpoints } from './ApiEndpoints';
+import Spinner from '../../helpers/spinner';
 
 export const TaggedOperations = ({ orderedOperations, ...props }) => {
-	return orderedOperations.map((taggedOperations, i) => (
-		<ApiEndpoints
-			key={i}
-			tag={taggedOperations.tag}
-			taggedOperations={taggedOperations}
-			{...props}
-		/>
-	));
+	return orderedOperations.length > 0 ? (
+		orderedOperations.map((taggedOperations, i) => (
+			<ApiEndpoints
+				key={i}
+				tag={taggedOperations.tag}
+				taggedOperations={taggedOperations}
+				{...props}
+			/>
+		))
+	) : (
+		<Spinner />
+	);
 };
