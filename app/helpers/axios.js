b1f1bc1f9b428b310338b7fa10e86ece60914e97
Andrew Bailey <alb@andrewbailey.me>
2020-05-26 09:09:13 -0400

    Repo clean up (#53)
    * remove empty file and parent dir
    
    * remove unused file
    
    * remove unused file
    
    * remove comment
    
    * remove unused file
    
    * remove comments
    
    * remove comments
    
    * remove comment
    
    * remove unused imports

--- app/helpers/axios.js
+++ app/helpers/axios.js
@@ -1,21 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const AxiosV1 = axios.create({
-  baseURL: `${window.API_BASE}v1/`,
-  timeout: 20000,
-  withCredentials: true
+	baseURL: `${window.API_BASE}v1/`,
+	timeout: 20000,
+	withCredentials: true
 });
 
 export const AxiosV2 = axios.create({
-  baseURL: `${window.API_BASE}v2/`,
-  timeout: 20000,
-  withCredentials: true
+	baseURL: `${window.API_BASE}v2/`,
+	timeout: 20000,
+	withCredentials: true
 });
 
 export const PublicAPI = axios.create({
-  baseURL: `${window.window.PUB_API_BASE}`,
-  timeout: 20000
-  // withCredentials: true
+	baseURL: `${window.window.PUB_API_BASE}`,
+	timeout: 20000
 });
 
 export default AxiosV1;
--- app/reducers/systemstatus.js
+++ app/reducers/systemstatus.js
@@ -1,33 +1,30 @@
-import Map from "lodash/map";
-import Flatten from "lodash/flatten";
-
 const initialState = {
-  fetched: false,
-  fetching: false,
-  status: {},
-  lastUpdated: {}
+	fetched: false,
+	fetching: false,
+	status: {},
+	lastUpdated: {}
 };
 
 export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "UPDATE_SSTATUS":
-      state = Object.assign({}, state, action.payload);
-      break;
-    case "CLEAR_SSTATUS":
-      state = Object.assign({}, initialState);
-      break;
-    case "FETCHING_SSTATUS":
-      state = Object.assign({}, state, { fetching: true });
-      break;
-    case "FETCHED_SSTATUS":
-      state = Object.assign({}, state, {
-        fetched: true,
-        fetching: false,
-        status: action.payload,
-        lastUpdated: Date.now() - action.payload.lastStatusCheck
-      });
-      break;
-  }
+	switch (action.type) {
+		case 'UPDATE_SSTATUS':
+			state = Object.assign({}, state, action.payload);
+			break;
+		case 'CLEAR_SSTATUS':
+			state = Object.assign({}, initialState);
+			break;
+		case 'FETCHING_SSTATUS':
+			state = Object.assign({}, state, { fetching: true });
+			break;
+		case 'FETCHED_SSTATUS':
+			state = Object.assign({}, state, {
+				fetched: true,
+				fetching: false,
+				status: action.payload,
+				lastUpdated: Date.now() - action.payload.lastStatusCheck
+			});
+			break;
+	}
 
-  return state;
+	return state;
 }
